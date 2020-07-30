b5e7fa6784f5cdff4b5459f140291fe56ff633e8
csjv95 <csjv95@gmail.com>
2020-07-30 12:42:40 +0900

    feat : js => innerText

--- web APIs/coordinate/coordinate.js
+++ web APIs/coordinate/coordinate.js
@@ -1,9 +1,18 @@
 'use strict';
 
 const middle = document.querySelector( '.middle');
+
+// when window clicked and show client x,y ,page x,y
 window.addEventListener('click' , (e) => {
   const client = `client X = ${e.clientX} clinet Y = ${e.clientX}`;
+  const page = `page x = ${e.pageX} page Y = ${e.pageY}`;
+  e.target.innerText = `${client}, ${page}`
   console.log(client);
+  console.log(page);
+});
+
+// when middle clicked and show rect 
+middle.addEventListener('click', (e) => {
   const rect = middle.getBoundingClientRect();
   console.log(rect);
-})
+});
