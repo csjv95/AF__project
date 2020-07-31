b4f5f6b5f59698d10550517c468be7a363bd4750
csjv95 <csjv95@gmail.com>
2020-07-31 16:20:16 +0900

    feat : when mouse move together move horizontal,vertical,target(img),coordinateX,Y

--- web APIs/findcoordinate/find.js
+++ web APIs/findcoordinate/find.js
@@ -1,6 +1,16 @@
 `use strick`
+const horizontal = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
 const cordinateXY = document.querySelector('.discription');
+const target = document.querySelector('.target');
 
 document.addEventListener('mousemove', (e) => {
-  cordinateXY.innerText = `Client X/Y: ${e.clientX}, ${e.clientY}`;
+  //style을 할때는 px이라는 유닛을 붙쳐야한다
+  horizontal.style.top =`${e.clientY}px`; //e.clientY에 px값을 전달해준다
+  vertical.style.left =`${e.clientX}px`;
+  target.style.top =`${e.clientY}px`;
+  target.style.left =`${e.clientX}px`;
+  cordinateXY.style.top =`${e.clientY}px`;
+  cordinateXY.style.left =`${e.clientX}px`;
+  cordinateXY.innerText = `${e.clientX}, ${e.clientY}`;
 });
