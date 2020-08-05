c753b175338ee862ce8a2b12f454e99ea5841dec
csjv95 <csjv95@gmail.com>
2020-08-05 14:56:48 +0900

    feat : Get better fast rendering! refer to rendering.md or some website

--- web APIs/findcoordinate/find.js
+++ web APIs/findcoordinate/find.js
@@ -1,16 +1,20 @@
 `use strick`
 const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
-const cordinateXY = document.querySelector('.discription');
 const target = document.querySelector('.target');
+const cordinateXY = document.querySelector('.discription');
 
 document.addEventListener('mousemove', (e) => {
   //style을 할때는 px이라는 유닛을 붙쳐야한다
-  horizontal.style.top =`${e.clientY}px`; //e.clientY에 px값을 전달해준다
-  vertical.style.left =`${e.clientX}px`;
-  target.style.top =`${e.clientY}px`;
-  target.style.left =`${e.clientX}px`;
-  cordinateXY.style.top =`${e.clientY}px`;
-  cordinateXY.style.left =`${e.clientX}px`;
-  cordinateXY.innerText = `${e.clientX}, ${e.clientY}`;
+  const x = e.clientX;
+  const y = e.clientY;
+  const targetRect = target.getBoundingClientRect();
+  const targetWidth = targetRect.width /2;
+  const targetHeight = targetRect.height / 2;
+  
+  horizontal.style.transform =`translateY(${y}px)`; //e.clientY에 px값을 전달해준다
+  vertical.style.transform =`translateX(${x}px)`;
+  target.style.transform =`translate(${x-targetWidth}px, ${y-targetHeight}px)`;
+  cordinateXY.style.transform =`translate(${x}px, ${y}px)`;
+  cordinateXY.innerText = `${x}, ${y}`;
 });
