8479034aae139f44d71df6c540e729850254853c
csjv95 <csjv95@gmail.com>
2020-08-17 21:57:02 +0900

    docs : renewal shopping list

--- shoppingList/shopping.js
+++ shoppingList/shopping.js
@@ -1,64 +1,67 @@
 'use strick'
 
+// when clicked the plus btn
 const itemList = document.querySelector('.item__list');
-const item = document.querySelector('.item');
+const plusBtn = document.querySelector('.plus__btn');
+const input = document.querySelector('.get__write');
 
-const input = document.querySelector('.footer__input');
-const clickBtn = document.querySelector('.puls__btn');
 
-//When click add btn
-clickBtn.addEventListener('click', ()=> {
-  plusBtn();
-});
 
-// when you press enter in the input 
-input.addEventListener('keydown' , (e) => {
-  if(e.keyCode == 13){
-    // enter key 받 았을때 입력된값으로 만들기
-    console.log('keydown in input');
-  }
+
+plusBtn.addEventListener('click', () => {
+  plus();
 })
 
-// plus btn
-const plusBtn = () => {
-    //get input value
-    const text = input.value;
-    console.log(text);
+const plus = () => {
+  // get user input value
+  const inputValue = input.value;
 
-    //make item list
-    const item = createItem(text);
-    itemList.appendChild(item);
-    
-    //reset input & focus
-    input.value = '';
+  if(inputValue === '') {
     input.focus();
-}
+    alert('Plz typing');
+    return;
+  }
+  //create new item
+  const itemRow = createItem(inputValue);
 
-const createItem = (text) => {
+  //add item in itemContainer
+  itemList.appendChild(itemRow);
+
+  //input default
+  input.value = '';
+  focus();
+  
+};
+
+const createItem = (inputValue) => {
   const item = document.createElement('li');
-  item.setAttribute ('class' , 'item');
+  item.setAttribute('class','item');
 
   const itemContainer = document.createElement('div');
-  itemContainer.setAttribute('class', 'item__container')
+  itemContainer.setAttribute('class','item__container' );
 
-  const itemName = document.createElement('span');
-  itemName.setAttribute('class','item__name');
-  itemName.innerText = text;
+  const name = document.createElement('span');
+  name.setAttribute('class','item__name');
+  name.innerText = inputValue;
 
-  const delBtn = document.createElement('button');
-  delBtn.setAttribute('class' , 'item__delete');
-  delBtn.innerHTML = '<i class="fas fa-trash"></i>';
-  delBtn.addEventListener('click', () => {
+  const itemDelete = document.createElement('button');
+  itemDelete.setAttribute('class','item__delete');
+  itemDelete.innerHTML = ' <i class="far fa-trash-alt"></i>';
+
+  itemDelete.addEventListener('click', () => {
     itemList.removeChild(item);
-  });
+  })
 
-  const underLine = document.createElement('div');
-  underLine.setAttribute('class','underLine');
+  const line = document.createElement('div');
+  line.setAttribute('class','line');
+
+  itemList.appendChild(item);
 
   item.appendChild(itemContainer);
-  item.appendChild(underLine);
-  itemContainer.appendChild(itemName);
-  itemContainer.appendChild(delBtn);
+  item.appendChild(line);
+
+  itemContainer.appendChild(name);
+  itemContainer.appendChild(itemDelete);
 
   return item;
-};
+}
