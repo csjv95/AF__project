35be3bb2791a264948d5de2b02433a9212f5e20b
csjv95 <csjv95@gmail.com>
2020-09-04 23:34:20 +0900

    feat : add fuc that when clicked the start btn,stop and startGame have setGameFeild,changeStartBtn,showGameBox,count

--- findCarrot/find.js
+++ findCarrot/find.js
@@ -1,11 +1,23 @@
 `use strict`
+const CARROT__SIZE = 80;
+let CARROT__COUNT = 10;
+let GAME__DURATION = 5;
+let stopWatch = undefined;
+let started = false;
 const startBtn = document.querySelector('.game__start__btn');
+const gameCount =document.querySelector('.game__count');
+const carrotCount = document.querySelector('.carrot__count');
 const gameField = document.querySelector('.game__field');
 const rectGameField = gameField.getBoundingClientRect();
 
+//when clicked start btn
 startBtn.addEventListener('click', () => {
-  startGame();
-  count();
+  if(started) {
+    stopGame();
+  }else {
+    startGame();
+  }
+  started = !started;
 });
 
 // remove target
@@ -18,19 +30,41 @@ gameField.addEventListener ('click', (e) => {
 
 //start game
 function startGame() {
+  setGameFeild();
+  changeStartBtn()
+  showGameBox();
+  count();
+}
+function setGameFeild() {
+  // reset gameFeild
+  gameField.innerHTML ='';
+  gameCount.innerText = GAME__DURATION;
   addItem('carrot','10','./img/carrot.png','absolute');
   addItem('bug','10','./img/bug.png','absolute');
 }
 
-//add items carrot and bug
+//when started game show game box
+function showGameBox() {
+  gameCount.style.visibility ='visible';
+  carrotCount.style.visibility ='visible';
+}
+
+//when clicked the startBtn will be change as stop btn
+function changeStartBtn() {
+  const icon = startBtn.querySelector('.fa-grin-stars');
+  icon.classList.remove('fa-grin-stars');
+  icon.classList.add('fa-pause');
+  console.log(icon.classList);
+}
+
+//create carrot and bug on gameField
 function addItem (className,count,imgsPath,position) {
-  const carrotSize = 80;
-  //create carrot on gameField
   for(let i = 0; i < count; i++){
   const item = document.createElement('img');
 
-  const x = ranadomNum(rectGameField.left,rectGameField.right-carrotSize);
-  const y = ranadomNum(rectGameField.top,rectGameField.bottom-carrotSize);
+  //item territory
+  const x = ranadomNum(rectGameField.left,rectGameField.right-CARROT__SIZE);
+  const y = ranadomNum(rectGameField.top,rectGameField.bottom-CARROT__SIZE);
   
   item.setAttribute('class',className);
   item.setAttribute('src',imgsPath);
@@ -41,7 +75,7 @@ function addItem (className,count,imgsPath,position) {
 
   gameField.appendChild(item);
   }
-};
+}
 
 //random
 function ranadomNum(min, max) {
@@ -50,9 +84,22 @@ function ranadomNum(min, max) {
 
 //time
 function count() {
-  window.setTimeout(slowAlert, 3000);
-};
+  stopWatch = setInterval(gameTimer,1000);
+}
 
-function slowAlert() {
-  alert('Too late');
-};
+function gameTimer() {
+  if(GAME__DURATION <= 0){
+    gameCount.innerText = 'End';
+  }else {
+    gameCount.innerText = GAME__DURATION;
+    --GAME__DURATION;
+  }
+}
+
+function stopGame() {
+  stopGameTimer();
+}
+
+function stopGameTimer() {
+  clearInterval(stopWatch);
+}
