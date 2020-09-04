3ccba1ce59dfda159c7545ab2231618853f7e947
csjv95 <csjv95@gmail.com>
2020-09-04 15:12:55 +0900

    feat : startGame and radom,remove,time fnc

--- findCarrot/find.js
+++ findCarrot/find.js
@@ -1,25 +1,63 @@
-const starBtn = document.querySelector('.game__start__btn');
+`use strict`
+const startBtn = document.querySelector('.game__start__btn');
 const gameField = document.querySelector('.game__field');
+const rectGameField = gameField.getBoundingClientRect();
 
-//when clicked the start btn 
-starBtn.addEventListener('click', ()=> {
-  console.log('click');
+startBtn.addEventListener('click', () => {
   startGame();
+  count();
 });
 
-//start game!!!
 const startGame = () => {
-  gameField.innerHTML = `Hi`;
-  //좌표 랜덤하게 받아서 이미지 넣기!!
-  makeCarrot();
+  const carrotSize = 80;
+  //create carrot on gameField
+  for(let i = 0; i<11; i++){
+  const carrotImg = document.createElement('img');
+  const x = ranadomNum(rectGameField.left,rectGameField.right-carrotSize);
+  const y = ranadomNum(rectGameField.top,rectGameField.bottom-carrotSize);
 
+  carrotImg.setAttribute('src','./img/carrot.png');
+  carrotImg.style.position = 'absolute';
+ 
+  carrotImg.style.left =`${x}px`;
+  carrotImg.style.top =`${y}px`;
+
+  gameField.appendChild(carrotImg);
+  }
+  //create bug on gameField
+  for(let i = 0; i<11; i++){
+    const bugImg = document.createElement('img');
+    const x = ranadomNum(rectGameField.left,rectGameField.right-carrotSize);
+    const y = ranadomNum(rectGameField.top,rectGameField.bottom-carrotSize);
+
+    bugImg.setAttribute('src','./img/bug.png');
+    bugImg.style.position = 'absolute';
+  
+    bugImg.style.left =`${x}px`;
+    bugImg.style.top =`${y}px`;
+    
+    gameField.appendChild(bugImg);
+    }
 };
 
-const makeCarrot = () => {
-   //당근 넣기
-   const carrotImg = document.createElement('img');
-   carrotImg.src = './img/carrot.png';
-   gameField.appendChild(carrotImg);
-
+//random
+function ranadomNum(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
+// remove target
+gameField.addEventListener ('click', (e) => {
+  if(e.target === e.target) {
+    console.log('delte');
+    gameField.removeChild(e.target);
+  }
+});
+
+//time
+const count = () => {
+  window.setTimeout(slowAlert, 3000);
+};
+
+const slowAlert = ()=> {
+  alert('Too late');
+}
