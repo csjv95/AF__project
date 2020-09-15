708757d2bf955feb73c4ef2db3f6910af2c9e6ef
csjv95 <csjv95@gmail.com>
2020-09-15 23:21:23 +0900

    feat : add fuc => time count and carrot count

--- findCarrot/find.js
+++ findCarrot/find.js
@@ -1,9 +1,11 @@
 `use strict`
 const CARROT__SIZE = 80;
+
 let CARROT__COUNT = 10;
-let GAME__DURATION = 5;
+let GAME__DURATION = 10;
 let stopWatch = undefined;
 let started = false;
+let score = 0;
 
 const game = document.querySelector('.game');
 const startBtn = document.querySelector('.game__start__btn');
@@ -12,40 +14,74 @@ const carrotCount = document.querySelector('.carrot__count');
 const gameField = document.querySelector('.game__field');
 const rectGameField = gameField.getBoundingClientRect();
 const popUp = document.querySelector('.pop-up');
-const resumeBtn = document.querySelector('.pop-up__refresh');
+const restartBtn = document.querySelector('.pop-up__refresh');
+const gameCountNum = gameCount.querySelector('.fa-stopwatch')
+const remainBug = popUp.querySelector('.remain');
 
 //when clicked start btn
 startBtn.addEventListener('click', () => {
   if(started) {
     stopGame();
+    console.log(started);
   }else {
     startGame();
+    console.log(started);
   }
-  started = !started;
 });
 
-// remove target
+//when clicked the target after remove target
 gameField.addEventListener ('click', (e) => {
-  if(e.target === e.target) {
-    console.log('delte');
-    gameField.removeChild(e.target);
+  const target = e.target;
+  if(!started) {
+    return;
+  }
+  if(target.matches('.carrot')) {
+    score++;
+    target.remove();
+    updateScore();
+    if(CARROT__COUNT === score) {
+      stopGameTimer();
+      finishGame();
+    }
+  }else if(target.matches('.bug')) {
+    target.classList.add('js_catch__bug');
+    stopGameTimer();
+    finishGame();
   }
 });
 
-
+//when clicked the restart btn
+restartBtn.addEventListener('click', () => {
+  opacityUp();
+  popUpRemove();
+  startGame();
+});
 
 //start game
 function startGame() {
+  started = true;
+  score = 0;
+  playAudio();
   setGameFeild();
-  changeStartBtn()
+  changeStartBtn();
   showGameBox();
+  updateScore();
   count();
+}
+
+//stop game
+function stopGame() {
+  started = false;
+  stopGameTimer();
+  hiddenBtn();
+  opcityDown();
+  popUpShow();
 }
 
 function setGameFeild() {
   // reset gameFeild
   gameField.innerHTML ='';
-  gameCount.innerText = GAME__DURATION;
+  gameCountNum.innerText = ` ${GAME__DURATION} `;
   addItem('carrot','10','./img/carrot.png','absolute');
   addItem('bug','10','./img/bug.png','absolute');
 }
@@ -58,10 +94,10 @@ function showGameBox() {
 
 //when clicked the startBtn will be change as stop btn
 function changeStartBtn() {
-  const icon = startBtn.querySelector('.fa-grin-stars');
+  const icon = startBtn.querySelector('.fas');
   icon.classList.remove('fa-grin-stars');
   icon.classList.add('fa-pause');
-  console.log(icon.classList);
+  startBtn.style.visibility ='visible';
 }
 
 //create carrot and bug on gameField
@@ -96,23 +132,18 @@ function count() {
 
 function gameTimer() {
   if(GAME__DURATION <= 0){
-    gameCount.innerText = 'End';
+     gameCountNum.innerText = ` ${GAME__DURATION}`;
+    stopGameTimer();
+    finishGame();
   }else {
-    gameCount.innerText = GAME__DURATION;
+    gameCountNum.innerText = ` ${GAME__DURATION}`;
     --GAME__DURATION;
   }
 }
 
-function stopGame() {
-  stopGameTimer();
-  hiddenBtn();
-  opcityDown();
-  popUpShow();
-  
-}
-
 function stopGameTimer() {
   clearInterval(stopWatch);
+  GAME__DURATION = 5;
 }
 
 function hiddenBtn() {
@@ -120,10 +151,34 @@ function hiddenBtn() {
 }
 
 function popUpShow() {
-  popUp.style.visibility = 'visible';
+  popUp.classList.remove('hidden');
 }
 
+function popUpRemove() {
+  popUp.classList.add('hidden');
+}
+
+// gmae opcity
 function opcityDown() {
   game.style.opacity = '20%';
   popUp.style.opacity = '90%';
 }
+
+function opacityUp() {
+  game.style.opacity = '100%';
+}
+
+// update
+function updateScore() {
+  const boxIcon = document.querySelector('.fa-box-open');
+  boxIcon.innerText = ` ${CARROT__COUNT-score}`;
+}
+
+//finish game
+function finishGame() {
+  started = false;
+  remainBug.innerText = ` Remain Carrot :${CARROT__COUNT-score}`
+  opcityDown();
+  popUpShow();
+}
+
